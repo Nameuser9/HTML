47b7775dba9c9e4f7cec0482c25dae1fc34e36cf
Nameuser9 <ivamand2002@gmail.com>
2024-02-28 12:32:47 +0700

    5 dz

--- js/script.js
+++ js/script.js
@@ -184,3 +184,118 @@ const dz3_task4 = function(){
         console.log(makeX(i));
     }
 }
+const dz4_task1 = function(){
+    const numbers = {
+        keyin1: 1,
+        keyin2: 2,
+        keyin3: 3,
+        keyin4: 4,
+        keyin5: 5,
+        keyin6: 6,
+        keyin7: 7,
+        }
+    for (let [key,value] of Object.entries(numbers)){
+        if (value>= 3){
+        console.log(value)}
+    }
+}
+const dz4_task2 = function(){
+    const post = {
+        author: "John", // вывести этот текст
+        postId: 23,
+            comments: [
+                {
+                userId: 10,
+                userName: "Alex",
+                text: "lorem ipsum",
+                    rating: {
+                    likes: 10,
+                    dislikes: 2, // вывести это число
+                    },
+                },
+                {
+                userId: 5, // вывести это число
+                userName: "Jane",
+                text: "lorem ipsum 2", // вывести этот текст
+                    rating: {
+                    likes: 3,
+                    dislikes: 1,
+                    },
+                },
+            ],
+        };
+        console.log(post.author)
+        console.log(post.comments[0].rating.dislikes)
+        console.log(post.comments[1].userName)
+        console.log(post.comments[1].text)
+}
+const dz4_task3 = function(){
+    const products = [
+            {
+                id: 3,
+                price: 200,
+            },
+            {
+                id: 4,
+                price: 900,
+            },
+            {
+                id: 1,
+                price: 1000,
+            },
+        ];
+        products.forEach(function(products) { 
+            products.price*=0.85;
+            console.log(products);
+            
+        });
+}
+
+const dz4_task4 = function(){
+const products = [
+        {
+        id: 3,
+        price: 127,
+            photos: [
+            "1.jpg",
+            "2.jpg",
+            ],
+            },
+            {
+        id: 5,
+        price: 499,
+        photos: [],
+        },
+        {
+        id: 10,
+        price: 26,
+            photos: [
+            "3.jpg",
+            ],
+        },
+        {
+            id: 8,
+            price: 78,
+        },
+        ];
+        let products_sorted = products.filter(function(item){
+            if (item.photos != ""){
+                if (item.photos != undefined){
+                    return true;
+                }
+            }
+        });
+        
+        products_sorted = products_sorted.sort(products_sorted.price)
+        products_sorted2 = products.sort((x, y) => x.price - y.price);
+        console.log(products_sorted)
+        console.log(products_sorted2)
+}
+
+const dz4_task5 = function(){
+    const en = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+    const ru = ["понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"];
+    let new_obj ={};
+    en.map((item, index) => new_obj[item] = ru[index])
+    console.log(new_obj)
+}
