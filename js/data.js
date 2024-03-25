994ccad3c65a6937e2d9f9c835d5c8e3f0b7009f
Nameuser9 <ivamand2002@gmail.com>
2024-03-25 16:55:10 +0700

    js2-5-2

--- js/data.js
+++ js/data.js
@@ -3,7 +3,8 @@ const newBloc = ` [
     "name": "Molecule Man",
     "age": 29,
     "secretIdentity": "Dan Jukes",
-    "powers": ["Radiation resistance", "Turning tiny", "Radiation blast"]
+    "powers": ["Radiation resistance", "Turning tiny", "Radiation blast"],
+    "img" : "https://media-cdn.sygictraveldata.com/media/1200x630/612664395a40232133447d33247d3833333830383635"
   },
   {
     "name": "Madame Uppercut",
@@ -13,7 +14,8 @@ const newBloc = ` [
       "Million tonne punch",
       "Damage resistance",
       "Superhuman reflexes"
-    ]
+    ],
+    "img" : "https://avatars.mds.yandex.net/i?id=b2abe77306c9cfd15e9b06fbe42d1022d299bdb7-10640163-images-thumbs&n=13"
   },
   {
     "name": "Eternal Flame",
@@ -26,6 +28,8 @@ const newBloc = ` [
       "Teleportation",
       "Interdimensional travel"
     ]
+    ,
+    "img" :"https://b-port.com/mediafiles/news/315dd/4c0b9443728e5730d9afba33664cc9701b9ae76d.jpg"
   }
 ]
   `
--- js/script.js
+++ js/script.js
@@ -4,11 +4,13 @@ const data = JSON.parse(newBloc);
   const divEl = document.createElement('div');
   const name = document.createElement('h1');
   const powers = document.createElement('p');
-  
+  const img = document.createElement('img');
   name.textContent = element.name;
   powers.textContent = element.powers;
+  img.src = element.img;
   divEl.appendChild(name);
   divEl.appendChild(powers);
+  divEl.appendChild(img);
   divEl.classList.add(element.class);
   container.appendChild(divEl);
   });
