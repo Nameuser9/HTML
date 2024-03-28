a8b1f1bb5a764e75e7a8689f0e18ae4854336f63
Nameuser9 <ivamand2002@gmail.com>
2024-03-28 22:29:45 +0700

    js2_6_1

--- js/data.js
+++ js/data.js
@@ -1,36 +1,25 @@
 const newBloc = ` [
-    {
-    "name": "Molecule Man",
-    "age": 29,
-    "secretIdentity": "Dan Jukes",
-    "powers": ["Radiation resistance", "Turning tiny", "Radiation blast"],
-    "img" : "https://media-cdn.sygictraveldata.com/media/1200x630/612664395a40232133447d33247d3833333830383635"
-  },
   {
-    "name": "Madame Uppercut",
-    "age": 39,
-    "secretIdentity": "Jane Wilson",
-    "powers": [
-      "Million tonne punch",
-      "Damage resistance",
-      "Superhuman reflexes"
-    ],
-    "img" : "https://avatars.mds.yandex.net/i?id=b2abe77306c9cfd15e9b06fbe42d1022d299bdb7-10640163-images-thumbs&n=13"
-  },
-  {
-    "name": "Eternal Flame",
-    "age": 1000000,
-    "secretIdentity": "Unknown",
-    "powers": [
-      "Immortality",
-      "Heat Immunity",
-      "Inferno",
-      "Teleportation",
-      "Interdimensional travel"
-    ]
-    ,
-    "img" :"https://b-port.com/mediafiles/news/315dd/4c0b9443728e5730d9afba33664cc9701b9ae76d.jpg"
-  }
-]
-  `
-  
+    "img":"img/man1.jpg",
+    "title3": "ELLERY X M'O CAPSULE",
+    "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi. ",
+        "price":"$52.00"
+},
+{
+  "img":"img/woman2FILL.png",
+  "title3": "ELLERY X M'O CAPSULE",
+  "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi. ",
+      "price":"$52.00"
+},
+{
+  "img":"img/man3.png",
+  "title3": "ELLERY X M'O CAPSULE",
+  "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi. ",
+      "price":"$52.00"
+},
+{
+  "img":"img/man4.png",
+  "title3": "ELLERY X M'O CAPSULE",
+  "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi. ",
+      "price":"$52.00"
+}]`
--- js/script.js
+++ js/script.js
@@ -2,15 +2,26 @@ const data = JSON.parse(newBloc);
   const container = document.querySelector('div.container');
   data.forEach(element => {
   const divEl = document.createElement('div');
-  const name = document.createElement('h1');
-  const powers = document.createElement('p');
   const img = document.createElement('img');
-  name.textContent = element.name;
-  powers.textContent = element.powers;
+  const title3 = document.createElement('h1');
+  const description = document.createElement('p');
+  const price = document.createElement('p');
+  const divE2 = document.createElement('div');
+  const button = document.createElement('a');
+  title3.textContent = element.title3;
+  description.textContent = element.description;
+  price.textContent = element.price
   img.src = element.img;
-  divEl.appendChild(name);
-  divEl.appendChild(powers);
+  button.href ='index.html'
+  divE2.classList.add("imgContainer");
+  divEl.appendChild(title3);
+  divEl.appendChild(description);
   divEl.appendChild(img);
+  divE2.appendChild(img);
+  button.appendChild(img);
+  divE2.appendChild(button);
   divEl.classList.add(element.class);
+  divEl.appendChild(divE2)
   container.appendChild(divEl);
+  
   });
