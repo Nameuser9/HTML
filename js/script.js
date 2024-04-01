b52cdb007b911f6d56c38bf8d7d9e4594a96189f
Nameuser9 <ivamand2002@gmail.com>
2024-04-01 22:40:37 +0700

    js2_7_1

--- js/script.js
+++ js/script.js
@@ -1,27 +1,69 @@
-const data = JSON.parse(newBloc);
-  const container = document.querySelector('div.container');
-  data.forEach(element => {
-  const divEl = document.createElement('div');
-  const img = document.createElement('img');
-  const title3 = document.createElement('h1');
-  const description = document.createElement('p');
-  const price = document.createElement('p');
-  const divE2 = document.createElement('div');
-  const button = document.createElement('a');
-  title3.textContent = element.title3;
-  description.textContent = element.description;
-  price.textContent = element.price
-  img.src = element.img;
-  button.href ='index.html'
-  divE2.classList.add("imgContainer");
-  divEl.appendChild(title3);
-  divEl.appendChild(description);
-  divEl.appendChild(img);
-  divE2.appendChild(img);
-  button.appendChild(img);
-  divE2.appendChild(button);
-  divEl.classList.add(element.class);
-  divEl.appendChild(divE2)
-  container.appendChild(divEl);
-  
-  });
+const container = document.querySelector('div.container');
+
+const videoEl = document.createElement('video');
+videoEl.src = 'img/sov.mp4';
+videoEl.style.width = '360px';
+container.appendChild(videoEl);
+
+const uiDiv = document.createElement('div');
+uiDiv.classList.add('ui');
+container.appendChild(uiDiv);
+
+const progressEl = document.createElement('input');
+progressEl.type = 'range';
+progressEl.classList.add('progress');
+progressEl.style.width = '360px';
+progressEl.min = 0;
+progressEl.max = 100;
+progressEl.value = 0;
+uiDiv.appendChild(progressEl);
+
+const timeEl = document.createElement('p');
+timeEl.innerHTML = '0';
+uiDiv.appendChild(timeEl);
+
+const brEl = document.createElement('br');
+uiDiv.appendChild(brEl);
+
+const playEl = document.createElement('img');
+playEl.src='img/play.png'
+playEl.classList.add('fas');
+playEl.classList.add('fa-play');
+uiDiv.appendChild(playEl);
+
+const pauseEl = document.createElement('img');
+pauseEl.src='img/pause.png'
+pauseEl.classList.add('fas');
+pauseEl.classList.add('fa-pause');
+uiDiv.appendChild(pauseEl);
+
+const volumeEl = document.createElement('input');
+volumeEl.classList.add('volume');
+volumeEl.type = 'range';
+volumeEl.min = 0;
+volumeEl.max = 100;
+volumeEl.value = 10;
+volumeEl.step=0.5;
+uiDiv.appendChild(volumeEl);
+
+
+volumeEl.addEventListener('change', (e) => {
+videoEl.volume = e.target.value / 10;
+});
+
+playEl.addEventListener('click', (e) => {
+videoEl.play();
+});
+
+pauseEl.addEventListener('click', (e) => {
+videoEl.pause();
+});
+
+progressEl.addEventListener('change', (e) => {
+videoEl.currentTime = videoEl.duration * (e.target.value / 100);
+});
+
+videoEl.addEventListener('timeupdate', function (e) {
+timeEl.innerText = videoEl.currentTime;
+progressEl.value = Math.round(videoEl.currentTime / videoEl.duration * 100);
+});
